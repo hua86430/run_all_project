3d366a7e6e53b40911ec28b02478c6205aa78428
Hua <k0981465867@gmail.com>
2024-07-30 23:50:09 +0800

    extract build message method

---
+++ src/composables/ListenBuildProjectMessage.ts
@@ -0,0 +1,11 @@
+﻿import { Ref } from "vue";
+
+export function listenBuildProjectMessage(
+  buildMessage: Ref<string>,
+  projectName: string,
+) {
+  const listener = (event: any, message: string) => {
+    buildMessage.value = message + "\n";
+  };
+  window.ipcRenderer.on(`${projectName}-build-output`, listener);
+}
