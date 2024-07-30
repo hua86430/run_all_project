91a001d1378a61fc732168916e2a2668bc9e8dd6
Hua <k0981465867@gmail.com>
2024-07-30 22:36:41 +0800

    chore: remove icon

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,6 @@ async function showLogs() {
 
 <template>
   <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img
-        src="./assets/electron.svg"
-        class="logo electron"
-        alt="Electron logo"
-      />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-
     <button title="click me" @click="showLogs">click me to show log</button>
   </div>
 </template>
