531e017ba405104c99ae3658d9bb3661ec04e405
Hua <k0981465867@gmail.com>
2024-07-31 21:37:21 +0800

    run project after build

--- electron/main/buildAndRunProject.ts
+++ electron/main/buildAndRunProject.ts
@@ -7,29 +7,67 @@ export function buildAndRunProject() {
     const buildCommand = `dotnet build "${request.csprojFilePath}"`;
     const runCommand = `dotnet run --project "${request.csprojFilePath}"`;
 
-    try {
-      await new Promise((resolve, reject) => {
-        exec(buildCommand, (error, stdout, stderr) => {
-          if (error) {
-            event.sender.send(
-              `${request.projectName}-build-output`,
-              `Build error: ${error.message}`,
-            );
-            reject(error);
-          }
-          if (stderr) {
-            event.sender.send(
-              `${request.projectName}-build-output`,
-              `Build stderr: ${stderr}`,
-            );
-            reject(stderr);
-          }
+    function buildProject(
+      reject: (reason?: any) => void,
+      resolve: (value: PromiseLike<unknown> | unknown) => void,
+    ) {
+      exec(buildCommand, (error, stdout, stderr) => {
+        if (error) {
           event.sender.send(
             `${request.projectName}-build-output`,
-            `Build stdout: ${stdout}`,
+            `Build error: ${error.message}`,
           );
-          resolve(stdout);
-        });
+          reject(error);
+        }
+        if (stderr) {
+          event.sender.send(
+            `${request.projectName}-build-output`,
+            `Build stderr: ${stderr}`,
+          );
+          reject(stderr);
+        }
+        event.sender.send(
+          `${request.projectName}-build-output`,
+          `Build stdout: ${stdout}`,
+        );
+        resolve(stdout);
+      });
+    }
+
+    function runProject(
+      reject: (reason?: any) => void,
+      resolve: (value: PromiseLike<unknown> | unknown) => void,
+    ) {
+      exec(runCommand, (error, stdout, stderr) => {
+        if (error) {
+          event.sender.send(
+            `${request.projectName}run-output`,
+            `Run error: ${error.message}`,
+          );
+          reject(error);
+          return;
+        }
+        if (stderr) {
+          event.sender.send(
+            `${request.projectName}run-output`,
+            `Run stderr: ${stderr}`,
+          );
+
+          reject(stderr);
+          return;
+        }
+        event.sender.send(
+          `${request.projectName}run-output`,
+          `Run stdout: ${stdout}`,
+        );
+        resolve(stdout);
+      });
+    }
+
+    try {
+      await new Promise((resolve, reject) => {
+        buildProject(reject, resolve);
+        runProject(reject, resolve);
       });
     } catch (error) {
       return error;
